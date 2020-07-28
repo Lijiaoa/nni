cd7b1d68e1e54af138348588525afc73b6e48d4d
Lijiao <15910218274@163.com>
2020-07-28 14:05:27 +0000

    update

--- src/webui/prettier.config.js
+++ src/webui/prettier.config.js
@@ -1,13 +1,10 @@
 module.exports = {
   printWidth: 120,
   semi: true,
-  tabWidth: 4,
   trailingComma: 'none',
   bracketSpacing: true,
   eslintIntegration: true,
-  // Trailing commas help with git merging and conflict resolution
   trailingComma: 'all',
-  // Use single quote in all files. https://github.com/prettier/prettier/issues/1080#issuecomment-390363232
   singleQuote: true,
   jsxSingleQuote: true
 };
