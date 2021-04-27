5e118d3fc92b99bad2f13648666e601fdae08fbe
Lijiao <Lijiaoa@outlook.com>
2021-04-27 06:14:31 +0000

    update

--- ts/webui/config/webpack.config.js
+++ ts/webui/config/webpack.config.js
@@ -357,6 +357,9 @@ module.exports = function(webpackEnv) {
                       },
                     },
                   ],
+                  // [
+                  //   require.resolve('antd')
+                  // ],
                 ],
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
